e489283f75ac2c83c20fe7d13df66e0cc7ea7bed
hyper-mage <mlynn808138@gmail.com>
2023-09-18 09:51:59 -0500

    First set of rules

--- Roman Numerals Encoder/RomanNumeralsEncoder.py
+++ Roman Numerals Encoder/RomanNumeralsEncoder.py
@@ -14,6 +14,27 @@
 def solution(n):
     # TODO convert int to roman string
     rn = ''
-    
+    while n != 0:
+        if n >= 1000:
+            rn += 'M'
+            n -= 1000
+        elif n >= 500:
+            rn += 'D'
+            n -= 500
+        elif n >= 100:
+            rn += 'C'
+            n -= 100
+        elif n >= 50:
+            rn += 'L'
+            n -= 50
+        elif n >= 10:
+            rn += 'X'
+            n -= 10
+        elif n >= 5:
+            rn += 'V'
+            n -= 5
+        elif n >= 1:
+            rn += 'I'
+            n -= 1
             
     return
