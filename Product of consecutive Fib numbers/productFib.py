8607a06b0aa41de11e5cd77a9f0fc3ab04f6e5cf
Hyper-Mage <mlynn808138@gmail.com>
2023-09-19 22:22:27 -0500

    thoughts

--- Product of consecutive Fib numbers/productFib.py
+++ Product of consecutive Fib numbers/productFib.py
@@ -1,3 +1,8 @@
 def productFib(prod):
     # your code
     return 
+
+### thoughts ###
+# I remember doing a fibonacci function in my earlier years of college
+# The method used for that project was recursion and the base cases being the 0 and 1 for fibonacci numbers
+# I'll see if I can recall the same methods then to apply here if applicable
